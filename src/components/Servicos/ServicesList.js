9e66e319f4673b74d9f9343bd3022a80fdd0f95b
Rayane Reis <rayanereismota2@gmail.com>
2024-07-11 00:25:03 -0300

    feat: Complementar textos dos serviços

--- src/components/Servicos/ServicesList.js
+++ src/components/Servicos/ServicesList.js
@@ -16,7 +16,17 @@ const services = [{
         icon: appsIcon,
         title: 'Desenvolvimento de Aplicativos',
         description: 'Aplicativos desktop e mobile, para melhor servir a sua aplicação, se necessário',
-        details: 'Criamos aplicativos móveis para Android e iOS, focando em desempenho e usabilidade. Nossas tecnologias são React Native e Flutter.',
+        details: <>
+        Criamos aplicativos móveis para Android e iOS, focando em desempenho e usabilidade. Nossas tecnologias são React Native e Flutter.
+        <ul>
+            <li>
+                <span className='emphasis'>Interface Intuitiva: </span> Desenvolvemos o design do aplicativo, usando ferramentas como o Figma, assegurando acessibilidade e boa experiência do usuário.
+            </li>
+            <li>
+                <span className='emphasis'>Soluções Sob Medida: </span> Seja gerenciar estoque, agendar serviços ou rastrear entregas, entre outros, procuramos compreender a sua necessidade para oferecer a melhor estratégia.
+            </li>
+        </ul>
+        </>,
         image: appsImg
     }, 
     {
@@ -24,7 +34,17 @@ const services = [{
         icon: webIcon,
         title: 'Desenvolvimento Web',
         description: 'Criação e gerenciamento de sites estáticos e dinâmicos',
-        details: 'Sites e aplicações web desde páginas institucionais até e-commerces complexos. Nossas tecnologias e frameworks são HTML5, CSS3, JavaScript, React e Angular.',
+        details: <> <ul>
+            <li>
+                <span className='emphasis'>Responsividade:</span> Criamos sites que se adaptam a dispositivos móveis, tablets e desktops.
+            </li>
+            <li>
+                <span className='emphasis'>Personalização:</span> Personalizamos o design, cores e funcionalidades para atender às suas necessidades.
+            </li>
+            <li>
+                <span className='emphasis'>Funcionalidades de Back e Front-end:</span> Para além da interface, implementamos gerenciamento de conteúdo, formulários, integrações com APIs e mais.
+            </li>
+        </ul> </>,
         image: webImg
     },
     {
@@ -32,15 +52,39 @@ const services = [{
         icon: sistemasIcon,
         title: 'Modelagem de Sistemas',
         description: 'Planejamento do seu produto, de acordo com as etapas da engenharia de software',
-        details: 'Modelagem de sistemas complexos, com diagramas e especificações que descrevem detalhadamente a sua estrutura. Utilizamos UML e outras ferramentas para garantir uma visão clara e estruturada',
+        details: <> <ul>
+        <li>
+            <span className='emphasis'>Análise de Requisitos: </span>Identificamos e documentamos suas necessidades para garantir que o software atenda às suas expectativas.
+        </li>
+        <li>
+            <span className='emphasis'>Modelagem UML: </span>Criamos diagramas com casos de uso, classes, sequências, eatividades para visualizar e planejar o sistema.  
+        </li>
+        <li>
+            <span className='emphasis'>Design Arquitetural: </span>Desenvolvemos uma arquitetura eficiente  para garantir escalabilidade e segurança do software.
+        </li>
+        <li>
+            <span className='emphasis'>Documentação Completa: </span>Fornecemos documentação para facilitar manutenção e atualizações.
+        </li>
+    </ul> </>,
         image: sistemasImg
     },
     {
         id: 'consultoria',
         icon: consultoriaIcon,
         title: 'Consultoria',
-        description: 'Consultoria técnica para identificar e corrigir bugs em sistemas existentes, além de criar ou melhorar a documentação.',
-        details: 'Manutenção e refatoração de código, afim de garantir ao produto melhor performance, manutenção facilitada, maior confiabilidade e redução de custos',
+        description: 'Consultoria técnica para identificar e corrigir bugs em sistemas existentes, além de trabalhar com a documentação.',
+        details: <> <ul>
+        <li>
+            <span className='emphasis'>Análise e Diagnóstico: </span>Avaliamos seu software para identificar oportunidades de melhoria.
+        </li>
+        <li>
+            <span className='emphasis'>Refatoração: </span>Reestruturamos seu código para torná-lo mais eficiente, legível e fácil de manter.  
+        </li>
+        <li>
+            <span className='emphasis'>Treinamento:  </span>Ministramos treinamento para sua equipe.
+        </li>
+
+    </ul> </>,
         image: consultoriaImg
     }];
 
