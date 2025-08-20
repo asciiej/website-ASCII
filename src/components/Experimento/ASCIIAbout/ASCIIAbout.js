6587fd8f3be1d02c1dea87a7956c5af4aa3a3ba0
guimileib <guilhermemileib@gmail.com>
2025-08-20 14:47:55 -0300

    fix: Fixing the about  container

--- src/components/Experimento/ASCIIAbout/ASCIIAbout.js
+++ src/components/Experimento/ASCIIAbout/ASCIIAbout.js
@@ -5,25 +5,18 @@ import styles from './ASCIIAbout.module.css';
 function ASCIIAbout() {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.header}>
-          <h2 className={styles.title}>ABOUT</h2>
-        </div>
-        
+      <div className={styles.leftSide}>
+        <h2 className={styles.title}>ABOUT</h2>
+      </div>
+      
+      <div className={styles.rightSide}>
         <div className={styles.mainText}>
           <div className={`${styles.typewriterContainer} typewriter-green`}>
-            <Typewriter
-              options={{
-                strings: [
-                  'LOREM IPSUM DOLOR SIT AMET NSECTET.',
-                  'LOREM MASSA VESTIBULUM ORNARE',
-                  'PURUS SIT.'
-                ],
-                autoStart: true,
-                loop: true,
-                delay: 75,
-                deleteSpeed: 50,
-                pauseFor: 2000,
+            <Typewriter // sem loop e 
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('Lorem ipsum dolor sit amet nsectet. Lorem massa vestibulum ornare purus sit.')
+                  .start();
               }}
             />
           </div>
